daf22b076cc553acf6c477068d6b086890fde977
meddhawi <meddhawi@gmail.com>
2022-03-07 13:44:23 +0700

    copy code from previous fd for redux state management

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import App from 'next/app'
+import withReduxStore from '../utils/withReduxStore'
+import {Provider} from 'react-redux'
+
+class MyApp extends App {
+  constructor(props){
+    super(props)
+  }
+  
+  componentDidMount = () => {}
+
+  render(){
+    const { Component, pageProps, reduxStore} = this.props
+
+    return(
+      <Provider store={reduxStore}>
+        <Component {...pageProps} />
+      </Provider>
+    )
+  }
 }
 
-export default MyApp
+export default withReduxStore(MyApp)
