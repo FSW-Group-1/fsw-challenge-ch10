beb2c1cd5440c108b4c5f0cabb76b04bc341c138
FAUZAN HIBATUR RAHMAN <2106221@dbc.co.id>
2022-03-08 21:07:35 +0700

    login-register

---
+++ pages/LoginPage.js
@@ -0,0 +1,82 @@
+import { Form, FormGroup, Label, Input, Container, Col, Button, Set} from 'reactstrap'
+import {Layout} from './Component/layout'
+
+class LoginPage extends Component{
+  static contextType = AuthContext; 
+  constructor(props){
+      super(props);
+
+      this.state ={};
+  }
+
+  
+export default function Login() {
+  set = name => event => {
+    // console.log(event.target.value)  
+    this.setState({[name]: event.target.value});
+}
+
+handleSubmit = async(event) => {
+    const { email, password}  = this.state;
+    const { history } = this.props
+    event.preventDefault();
+
+     // Validasi
+     if(!email || !password) return alert('Please insert missing credentials!')
+
+     // Register via Firebase
+     try {
+        await firebase.auth().signInWithEmailAndPassword(email, password)
+         history.push('/');
+     } catch(error) {
+         alert('Failed to Login')
+         console.log(error)
+     }
+}
+
+    return (
+      <Layout title="Login">
+        <Container>
+        <Col xs={8} sm={12} md={12} className="text-center pt-sm-5 pt-xl-5">
+            <h1>LOGIN</h1>                       
+        </Col>
+        
+        <Form /*inline onSubmit={this.handleSubmit}*/>
+          
+          <FormGroup>
+            <Label
+              for="exampleEmail"
+              hidden
+            >
+              Your Email
+            </Label>
+            <Input
+              id="exampleEmail"
+              name="email"
+              placeholder="Your Email"
+              type="email"
+            //   onChange={this.set('email')}
+            />
+          </FormGroup>
+          {' '}
+          <FormGroup>
+            <Label
+              hidden
+            >
+              Your Password
+            </Label>
+            <Input
+              name="password"
+              placeholder="Your Password"
+            //   onChange={this.set('password')}
+              type="password"
+            />
+            </FormGroup>
+          <a href='/forgot-password'>Forgot your password? Click me!</a><br/>
+          <Button className='btn-success'>
+            Login
+          </Button>
+        </Form>
+        </Container>
+        </Layout>
+    )}
