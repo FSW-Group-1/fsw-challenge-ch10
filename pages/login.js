35447bb0e6826ae76bc2cf2d4ac46dce5f8f10fb
FAUZAN HIBATUR RAHMAN <2106221@dbc.co.id>
2022-03-08 21:24:23 +0700

    login-register-new

---
+++ pages/login.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import { Redirect } from 'react-router-dom'
+
+import { Form, FormGroup, Label, Input, Container, Col} from 'reactstrap';
+import firebase from '../auth/firebase';
+import { AuthContext } from "../auth/Auth";
+import { Component} from './components/layout'
+
+class LoginPage extends Component{
+  static contextType = AuthContext; 
+  constructor(props){
+      super(props);
+
+      this.state ={};
+  }
+
+  set = name => event => {
+      // console.log(event.target.value)  
+      this.setState({[name]: event.target.value});
+  }
+
+  handleSubmit = async(event) => {
+      // const { email, password}  = this.state;
+      // const { history } = this.props
+      // event.preventDefault();
+
+      //  // Validasi
+      //  if(!email || !password) return alert('Please insert missing credentials!')
+
+      //  // Register via Firebase
+      //  try {
+      //     await firebase.auth().signInWithEmailAndPassword(email, password)
+      //      history.push('/');
+      //  } catch(error) {
+      //      alert('Failed to Login')
+      //      console.log(error)
+      //  }
+  }
+
+  render(){
+      const { currentUser } = this.context
+      if (!!currentUser) return <Redirect to="/" />
+      return(
+        <>
+        <Layout tittle="login">
+        <Col xs={8} sm={12} md={12} className="text-center pt-sm-5 pt-xl-5">
+            <h1>LOGIN</h1>                       
+        </Col>
+        <Container>
+        <Form inline onSubmit={this.handleSubmit}>
+          
+          <FormGroup>
+            <Label
+              for="exampleEmail"
+              hidden
+            >
+              Your Email
+            </Label>
+            <Input
+              id="exampleEmail"
+              name="email"
+              placeholder="Your Email"
+              type="email"
+              onChange={this.set('email')}
+            />
+          </FormGroup>
+          {' '}
+          <FormGroup>
+            <Label
+              hidden
+            >
+              Your Password
+            </Label>
+            <Input
+              name="password"
+              placeholder="Your Password"
+              onChange={this.set('password')}
+              type="password"
+            />
+            </FormGroup>
+          <a href='/forgot-password'>Forgot your password? Click me!</a><br/>
+          <Button className='btn-success'>
+            Login
+          </Button>
+        </Form>
+        </Container>
+        </Layout>
+      </>
+      )
+  }
+}
+
+export default LoginPage; 
