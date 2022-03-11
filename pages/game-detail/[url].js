2f5f64ffbf91735a98da1d85d618ce2aa70fb6f1
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-03-11 19:05:34 +0700

    Fetching data

--- pages/game-detail/[url].js
+++ pages/game-detail/[url].js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Component, useEffect, useState } from "react"
 import { useRouter } from "next/router";
 import { Layout } from '../components/layout';
 import { LoadingAnimation } from '../components/loadingAnimation_1'
@@ -9,9 +9,38 @@ import Link from 'next/link'
 
 import styles from '../../styles/GameDetail.module.css'
 
+import axios from 'axios'
+
 const GameDetail = () => {
     const router = useRouter()
     const { url } = router.query
+    let gameUrl = 'https://fsw-challenge-ch10-api-dev.herokuapp.com/api/gamedetail/1'
+    // gameUrl = gameUrl + url
+
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        try {
+            const config = {
+              headers: {
+                  authorization: `${localStorage.getItem('accessToken')}`,
+              },
+            }
+            axios.get(`https://fsw-challenge-ch10-api-dev.herokuapp.com/api/allgame`, config)
+              .then(res => {
+                this.setState({
+                    gameList: res.data.data,
+                    isLoading: false
+                })
+            })
+      
+          } catch (error) {
+            console.log(error)
+            this.setState({
+              isLoading: false
+            })
+          }
+    })
 
     return(
         <>
